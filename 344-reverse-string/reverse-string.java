b103c5b8e989fd15087259b0e45dac8a9ad9c152
Harshad Shelar <harshadshelar1760@gmail.com>
2025-12-31 10:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (54.13%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void Harsh(char[] s) {
+
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+
+    }
+
+    public void reverseString(char[] s) {
+        Harsh(s);
+    }
+}
