85c4612c037f6fe9d7a92b31d1f2bfc5298b931d
Harshad Shelar <harshadshelar1760@gmail.com>
2026-02-25 12:39:17 +0530

    Time: 4 ms (80.51%) | Memory: 46.6 MB (33.52%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+   
+  
+
+    public boolean isAnagram(String s, String t) {
+        
+        char ch[]=s.toCharArray();
+        char ch1[]=t.toCharArray();
+
+        Arrays.sort(ch);
+        Arrays.sort(ch1);
+
+        
+       return Arrays.equals(ch1,ch);
+    }
+}
