6b5d513bf3b47fd3ef365f8aeda652f11f3791c3
Harshad Shelar <harshadshelar1760@gmail.com>
2026-06-20 18:12:23 +0530

    Time: 17 ms (15.89%) | Memory: 74 MB (24.27%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,27 +1,17 @@
 class Solution {
-    public static int Harsh(int nums[]) {
-
-        HashSet<Integer> mp = new HashSet<>();
-        for (int key : nums) {
-            if (key > 0) {
-                mp.add(key);
-            }
-        }
-
-        int smallnumber = 1;
-        while (true) {
-
-            if (!mp.contains(smallnumber)) {
-                return smallnumber;
-            }
-            smallnumber++;
-        }
-
-    }
-
     public int firstMissingPositive(int[] nums) {
-        return Harsh(nums);
+        
+        int n=nums.length;
+        Arrays.sort(nums);
+        
+          int k = 1;
 
-       
+        for (int num : nums) {
+            if (num == k) {
+                k++;
+            }
+        }
+
+        return k;
     }
 }
