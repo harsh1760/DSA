fd9766efb16b75fbe9498d7a6a8fb0719809c1fc
Harshad Shelar <harshadshelar1760@gmail.com>
2026-06-30 14:45:39 +0530

    Time: 8 ms (36.53%) | Memory: 44.1 MB (86.94%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,28 +1,20 @@
 class Solution {
-
-public static String Harsh(String str) {
-		
-		int n=str.length();
-		String[] str1= str.trim().split("\\s+");		
-		int first=0;
-		int end=str1.length-1;
-		while(first<end) {
-			
-		String temp=str1[first];
-		str1[first]=str1[end];
-		str1[end]=temp;
-		
-		first++;
-		end--;
-			
-		}
-		
-		String result = String.join(" ", str1);
-        return result;
-	}
-
-
     public String reverseWords(String s) {
-        return Harsh(  s);
+        
+            String str[]=s.trim().split("\\s+");
+
+            int i=0;
+            int j=str.length-1;
+
+            while(i<j){
+                
+                String st=str[i];
+                str[i]=str[j];
+                str[j]=st;
+                i++;
+                j--;
+            }
+
+              return String.join(" ", str);
     }
 }
